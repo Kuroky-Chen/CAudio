1137f2333298546d610b204a162e03562f8e7556
Kuroky <5837907@qq.com>
2023-08-22 14:11:32 +0800

    perf: 缓冲区

--- src/code.js
+++ src/code.js
@@ -1,23 +1,38 @@
-
 import PCMPlayer from "pcm-player";
 
 class CAudio {
   constructor(config) {
     this.config = config;
+    const {
+      url,
+      params,
+      audioType,
+      token,
+      sampleRate,
+      bufferSize,
+      volume,
+      flushTime,
+      inputCodec,
+      channels,
+      bufferFirstWaitTime,
+      bufferIntervalWaitTime,
+      bufferIntervalPlayTime
+    } = config;
 
-    const { url, params, audioType, token,sampleRate, bufferSize, volume, flushTime, inputCodec, channels  } = config
-    
-    this.player = null
-    this.apiUrl = url || ''  // 请求地址
-    this.params = params || '' // 请求参数
-    this.audioType = audioType || 'wav' // 音频格式
-    this.token = token || ''  // 请求token
-    this.sampleRate = sampleRate || 16000 // 采样率
-    this.bufferSize = bufferSize || 16000; // 音频帧的缓冲区大小
-    this.volume = volume || 1 // 音频声音大小
-    this.flushTime = flushTime || 200 // 缓冲区刷新时间
-    this.channels = channels || 1  // 声道数
-    this.inputCodec = inputCodec || 'Int16' // 采样编码方式
+    this.player = null;
+    this.apiUrl = url || ""; // 请求地址
+    this.params = params || ""; // 请求参数
+    this.audioType = audioType || "wav"; // 音频格式
+    this.token = token || ""; // 请求token
+    this.inputCodec = inputCodec || "Int16"; // 采样编码方式
+    this.sampleRate = sampleRate || 16000; // 采样率
+    this.bufferSize = bufferSize || 6400; // 音频帧的缓冲区大小
+    this.bufferFirstWaitTime = bufferFirstWaitTime || 10000; // 缓冲池第一次等待时间
+    this.bufferIntervalWaitTime = bufferIntervalWaitTime || 5000; // 缓冲池后续始等待时间
+    this.bufferIntervalPlayTime = bufferIntervalPlayTime || 200; // 缓冲池音频片段播放时间
+    this.volume = volume || 1; // 音频声音大小
+    this.flushTime = flushTime || 200; // 缓冲区刷新时间
+    this.channels = channels || 1; // 声道数
 
     this.init();
   }
@@ -31,86 +46,73 @@ class CAudio {
 
       const signal = this.controller.signal;
 
-      // const buffer = new Uint8Array(this.bufferSize);
-      // let bufferIndex = 0;
-      const bufferPool = [];
       let timer;
 
-      function timerInit(time) {
+      const timerInit = time => {
         setTimeout(() => {
           timer = setInterval(() => {
             onPlay();
-          }, 200);
+          }, self.bufferIntervalPlayTime);
         }, time);
-      }
+      };
 
-      function onPlay() {
-        if (bufferPool.length > 6400) {
-          const temp = bufferPool.splice(0, 6400);
+      const onPlay = () => {
+        if (self.bufferPool.length > self.bufferSize) {
+          const temp = self.bufferPool.splice(0, self.bufferSize);
           self.player.feed(new Uint8Array(temp));
         } else {
           clearInterval(timer);
-          timerInit(5000);
+          timerInit(self.bufferIntervalWaitTime);
         }
-      }
+      };
 
-      fetch(this.apiUrl + '?' + this.params.toString(), {
+      fetch(this.apiUrl + "?" + this.params.toString(), {
         headers,
         signal
-      }).then(response => {
-        const reader = response.body.getReader();
-        return new ReadableStream({
-          start(controller) {
-            function pump() {
-              return reader.read().then(({ done, value }) => {
-                if (done) {
-                  controller.close();
-                  return;
-                }
-                controller.enqueue(value);
-                return pump();
-              });
-            }
-            return pump();
-          }
-        });
       })
-      .then(stream => {
-        const reader = stream.getReader();
-        return new Response(
-          new ReadableStream({
+        .then(response => {
+          const reader = response.body.getReader();
+          return new ReadableStream({
             start(controller) {
-              const read = () => {
+              function pump() {
                 return reader.read().then(({ done, value }) => {
                   if (done) {
                     controller.close();
                     return;
                   }
-                  // 处理每次获取的数据流（value），大小为不定
-                  // const data = new Uint8Array(value.buffer);
-                  // for (let i = 0; i < data.length; i++) {
-                  //   buffer[bufferIndex] = data[i];
-                  //   bufferIndex++;
-                  //   if (bufferIndex >= self.bufferSize) {
-                  //     // 缓冲区已满，可以进行播放操作
-                  //     self.player.feed(buffer);
-                  //     // 重置缓冲区索引
-                  //     bufferIndex = 0;
-                  //   }
-                  // }
-                  const normalArr = Array.from(value);
-                  bufferPool.push(...normalArr);
-                  return read();
+                  controller.enqueue(value);
+                  return pump();
                 });
               }
-              return read();
+              return pump();
             }
-          })
-        );
-      })
-      .catch(error => {
-        // 处理错误
-      });
+          });
+        })
+        .then(stream => {
+          const reader = stream.getReader();
+          return new Response(
+            new ReadableStream({
+              start(controller) {
+                const read = () => {
+                  return reader.read().then(({ done, value }) => {
+                    if (done) {
+                      controller.close();
+                      return;
+                    }
+                    const normalArr = Array.from(value);
+                    self.bufferPool.push(...normalArr);
+                    return read();
+                  });
+                };
+                timerInit(self.bufferFirstWaitTime);
+                return read();
+              }
+            })
+          );
+        })
+        .catch(error => {
+          console.error(error);
+        });
     } catch (err) {
       console.error(err);
     }
@@ -118,22 +120,21 @@ class CAudio {
 
   stop() {
     try {
-      this.player.pause()
+      this.player.pause();
       this.controller.abort();
-    }catch(err) {
-      console.warn(err)
+    } catch (err) {
+      console.warn(err);
     }
 
-    this.player = null
+    this.player = null;
 
-    this.init()
+    this.init();
   }
 
   init() {
     // 初始化逻辑
     this.controller = new AbortController(); // 控制器对象
-    this.audioBuffer = []; // 音频缓冲区
-
+    this.bufferPool = []; // 音频缓冲区
     // 初始化PCM
     this.player = new PCMPlayer({
       inputCodec: this.inputCodec,
